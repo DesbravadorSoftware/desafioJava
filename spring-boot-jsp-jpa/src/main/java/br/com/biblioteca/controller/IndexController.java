28b0106ce2f8f921a0de5e48e90c3eab2941d691
Wagner <wagner.parisoto@desbravador.com.br>
2022-12-01 14:05:36 -0300

    Atualizacao

--- spring-boot-jsp-jpa/src/main/java/br/com/biblioteca/controller/IndexController.java
+++ spring-boot-jsp-jpa/src/main/java/br/com/biblioteca/controller/IndexController.java
@@ -1,14 +1,15 @@
 package br.com.biblioteca.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/index")
-	String home() {
+	@GetMapping("/")
+	public String home(Model model, HttpServletRequest httpServletRequest) {
 		return "index";
 	}
-	
 }
